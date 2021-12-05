7bb71ad969c67abcd232475d6a1c55527034df36
William Taylor <me@willtaylor.info>
2021-12-05 21:09:16 +0000

    Day 1 Part 2

--- src/day01.rs
+++ src/day01.rs
@@ -7,3 +7,18 @@ pub fn part1(input: String) {
 
     println!("Total Increases: {}", increase_count);
 }
+
+pub fn part2(input: String) {
+    let input = input.lines().map(|l| l.parse().unwrap()).collect::<Vec<i32>>();
+
+    let window_totals = (0..input.len()-2)
+        .map(|i| &input[i..i+3])
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>();
+
+    let increase_count = window_totals.iter()
+        .zip(&window_totals[1..])
+        .fold(0, |acc, pair| if pair.1 > pair.0 {acc+1} else {acc});
+
+    println!("Total Increases: {}", increase_count)
+}
